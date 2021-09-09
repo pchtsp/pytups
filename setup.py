8ac27d9d3a17979fe53311dccbe3fc353984a557
pchtsp <pchtsp@gmail.com>
2021-09-09 22:21:55 +0200

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("requirements.txt", "r") as fh:
 
 kwargs = {
     "name": "pytups",
-    "version": "0.81.0",
+    "version": "0.81.1",
     "packages": packages,
     "description": "data wrangling for lists of tuples and dictionaries",
     "long_description": long_description,
